6435178007a3e7d3fda66d4462b34d2485c588d5
Abayomi Akinsola <olasunkami010@gmail.com>
2018-06-04 20:26:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 const app = "I don't do much."
+ var kittens = ['Milo', 'Otis', 'Garfield'];
+ 
+ function destructivelyAppendKitten(name){
+ //kittens = ['Milo', 'Otis', 'Garfield']
+  kittens.push("Ralph")
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift("Bob")
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+}
+/*
+function appendKitten(name) {
+  var newArray = [
+    kittens[0],
+    kittens[1],
+    kittens[2],
+    name
+  ];
+  return newArray;
+};*/
+function appendKitten(name){
+  var newArray = new Kittens[]
+   newArray.push("Broom")
+   return newArray
+   
+}
+
+function prependKitten(name){
+    kittens.pop("Broom")
+}
